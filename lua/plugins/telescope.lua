aed70795df70b3878910e468b026fd7e89323fd2
Sokmontrey <sythatsokmontrey@gmail.com>
2024-01-08 21:24:10 -0500

    telescoping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,17 @@
 return {
   "nvim-telescope/telescope.nvim", tag = '0.1.5',
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = true,
+  config = function ()
+    require('telescope').setup({
+      defaults = {
+        layout_strategy = 'center',
+        layout_config = {
+          height = 0.5,
+          width = 0.6,
+          prompt_position = 'bottom',
+        },
+        preview = false,
+      },
+    })
+  end
 }
