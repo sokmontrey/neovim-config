55a7d44b09fa4b699ed84671e45ea7eb2afe7d44
Sokmontrey <sythatsokmontrey@gmail.com>
2024-01-26 19:52:37 -0500

    tab line and a few other listchars

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,43 +1,31 @@
 local plugins = {
-  {
-    'nvim-tree/nvim-web-devicons',
-    config = true,
-  },
-  {
-    'akinsho/toggleterm.nvim',
-    lazy = false,
-    config = true,
-  },
-  {
-    'jxnblk/vim-mdx-js',
-  },
-  {
-    'preservim/vim-markdown',
-  },
-  {
-    'evanleck/vim-svelte',
-  },
-  {
-    'tpope/vim-fugitive',
-  },
-  {
-    'rstacruz/vim-closer',
-  },
-  {
-    'folke/zen-mode.nvim',
-    opts={
-      window = {
-        width = .7,
-        height = 1,
-        backdrop = 1,
-        options = {
-          number = false,
-          relativenumber = false,
-          cursorline = false,
-        },
-      },
+    {
+        'nvim-tree/nvim-web-devicons',
+        config = true,
     },
-  },
+    {
+        'akinsho/toggleterm.nvim',
+        lazy = false,
+        config = true,
+    },
+    {
+        'jxnblk/vim-mdx-js',
+    },
+    {
+        'evanleck/vim-svelte',
+    },
+    {
+        'tpope/vim-fugitive',
+    },
+    {
+        'rstacruz/vim-closer',
+    },
+    {
+        'norcalli/nvim-colorizer.lua',
+        config = function()
+            require('colorizer').setup()
+        end
+    },
 }
 
 return plugins
--- lua/vim-setup.lua
+++ lua/vim-setup.lua
@@ -1,4 +1,4 @@
-vim.cmd("set expandtab")
+-- vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
@@ -8,3 +8,9 @@ vim.cmd("set numberwidth=2")
 vim.cmd("hi! link EndOfBuffer Ignore")
 vim.cmd("set cursorline")
 vim.cmd("set signcolumn=yes")
+vim.cmd("set colorcolumn=80")
+vim.cmd("set linebreak")
+vim.opt.termguicolors = true
+
+vim.o.listchars = 'trail:-,nbsp:+,tab:▏ '
+vim.o.list = true
