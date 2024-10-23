8314fae6e1362ad7324ea9722bbec8be9d9345af
Sokmontrey <sythatsokmontrey@gmail.com>
2024-10-23 12:27:58 -0400

    combine every theme into a single lua

--- lua/themes/ayu.lua
+++
@@ -1,4 +0,0 @@
-return {
-	"Shatur/neovim-ayu",
-	priority = 1000,
-}
---
+++ lua/themes/themes.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"AlexvZyl/nordic.nvim",
+		priority = 1000,
+	},
+	{
+		"Shatur/neovim-ayu",
+		priority = 1000,
+	},
+	{
+		"tiagovla/tokyodark.nvim",
+		priority = 1000,
+	}
+}
