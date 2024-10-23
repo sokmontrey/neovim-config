4bcef2ddad45677e6a55332828a15fba059e33f7
Sokmontrey <sythatsokmontrey@gmail.com>
2024-10-23 12:28:26 -0400

    compile and run shortcut for some programming languages

--- lua/utils/build.lua
+++ lua/utils/build.lua
@@ -1,7 +1,7 @@
 local cmd = {
 	cpp = {
-		compile = function(name, ext) return "clang++ --debug " .. name .. "." .. ext .. " -o " .. name end,
-		run = function(name, _) return name end
+		compile = function(name, ext) return "clang++ --debug -DLOCAL " .. name .. "." .. ext .. " -o .\\out\\" .. name end,
+		run = function(name, _) return ".\\out\\" .. name end
 	},
 	cs = {
 		compile = function(_, _) return "dotnet run" end,
@@ -13,21 +13,21 @@ local function compile()
 	local file_ext = vim.fn.expand("%:e")
 	local file_name = vim.fn.expand('%:t:r')
 	local command = cmd[file_ext].compile(file_name, file_ext)
-	vim.cmd('5TermExec size=65 direction=vertical cmd="'.. command ..'"')
+	vim.cmd('TermExec size=65 direction=vertical cmd="'.. command ..'"')
 end
 
 local function run()
 	local file_ext = vim.fn.expand("%:e")
 	local file_name = vim.fn.expand('%:t:r')
 	local command = cmd[file_ext].run(file_name, file_ext)
-	vim.cmd('5TermExec size=65 direction=vertical cmd="'.. command ..'"')
+	vim.cmd('TermExec size=65 direction=vertical cmd="'.. command ..'"')
 end
 
 local function compile_and_run()
 	local file_ext = vim.fn.expand("%:e")
 	local file_name = vim.fn.expand('%:t:r')
 	local command = cmd[file_ext].compile(file_name, file_ext) .. " & " .. cmd[file_ext].run(file_name, file_ext)
-	vim.cmd('5TermExec size=65 direction=vertical cmd="'.. command ..'"')
+	vim.cmd('TermExec size=65 direction=vertical cmd="'.. command ..'"')
 end
 
 return {
