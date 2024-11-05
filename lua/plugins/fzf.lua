de64358ec96a21d90c71f40bbc08c8b8a55ec12b
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-05 14:18:02 -0500

    install fzf

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,8 +1,18 @@
 return {
-  "ibhagwan/fzf-lua",
-  { "junegunn/fzf", build = "install --bin" },
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("fzf-lua").setup({ })
-  end
+	{
+		"junegunn/fzf",
+		build = "install --bin",
+	},
+	-- {
+	-- 	"ibhagwan/fzf-lua",
+	-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- 	config = function()
+	-- 		require("fzf-lua").setup({
+	-- 			files = {
+	-- 				find_opts = [[-type f -not -path '*/\.git/*' -not -path '*/node_modules/*' -printf '%P\n']],
+	-- 				previewer = false,
+	-- 			},
+	-- 		})
+	-- 	end
+	-- }
 }
