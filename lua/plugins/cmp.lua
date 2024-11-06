5fc2a963f18ac97c35187706d9e3fd48582ebba2
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-06 17:39:09 -0500

    change snippets priority

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -61,8 +61,8 @@ return {
 				},
 			},
 			sources = {
-				{ name = "luasnip", priority = 1100 },
-				{ name = "nvim_lsp", priority = 1000 },
+				{ name = "luasnip", priority = 1000 },
+				{ name = "nvim_lsp", priority = 1100 },
 			},
 			mapping = cmp_mappings,
 		})
